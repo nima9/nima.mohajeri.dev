31add73ace00ab3c3f72e892380df9ac52595b1a
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-11 00:24:10 -0800

    Style: added prettier package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
@@ -8,5 +8,5 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
