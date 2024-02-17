0e1970853ba1f44f17b88736ab4cbd3436a0ce65
Nima Mohajeri <nima.mohajeri0@gmail.com>
2024-02-16 17:00:09 -0800

    Fix: got rid of custom color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,6 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      matrixGreen: "#b0f70d",
-    },
     extend: {
       fontFamily: {
         cageroll: ["Cageroll", "sans-serif"],
